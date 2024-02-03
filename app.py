dc9318b02959d32cc6892c8a73234046a407bd73
pedram.zamaninezhad <pzamaninejad@yahoo.com>
2024-02-03 20:15:45 +0330

    finishing user table

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask, template_rendered, request, jsonify
+from flask import Flask, request, jsonify
 from flask_migrate import Migrate
+
 from models import db, User
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '363c98df0c8a633364e269e1a91f64307bad1cada77e72624c648bc80be4946f'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:/Users/pzama/Desktop/flask-starter/flask.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS '] = False
 
@@ -18,15 +20,24 @@ def home():
     new_lname = request.form['last_name']
     new_email = request.form['email']
     new_phone = request.form['phone']
-    new_slug = new_fname + new_lname
+    new_slug = new_fname + '-' + new_lname
+    new_type = request.form['type']
+    new_affiliated_by = ''
+    new_location = None
+    new_address = ''
+    new_password = request.form['password'] + app.config['SECRET_KEY']
 
-    new_user = User(first_name=new_fname, last_name=new_lname, email=new_email, phone=new_phone, \
-                    slug=new_slug)
+    try:
+        new_user = User(first_name=new_fname, last_name=new_lname, email=new_email, phone=new_phone, \
+                        slug=new_slug, affiliated_by=new_affiliated_by, location=new_location, address=new_address,
+                        password=new_password, type=new_type)
 
-    db.session.add(new_user)
-    db.session.commit()
+        db.session.add(new_user)
+        db.session.commit()
+    except:
+        return jsonify({'msg': 'Your form has a problem'}), 400
+    return jsonify({'msg': 'created succesfully'}), 201
 
-    return jsonify({'msg': 'created succesfully'})
 
 app.app_context().push()
 
--- models.py
+++ models.py
@@ -1,25 +1,52 @@
-from flask_sqlalchemy import SQLAlchemy
 import uuid
 import datetime
+from enum import Enum
+
+from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+
+
 def generated_uuid():
     import string, random
     charecters = string.ascii_letters + string.digits
     short_uuid = ''.join(random.choice(charecters) for _ in range(10))
     return short_uuid
 
+
+class BloodType(Enum):
+    Not_given = 'NG'
+    A_positive = 'A+'
+    A_negative = 'A-'
+    B_positive = 'B+'
+    B_negative = 'B-'
+    O_positive = 'O+'
+    O_negative = 'O-'
+    AB_positive = 'AB+'
+    AB_negative = 'AB-'
+
+
+class Type(Enum):
+    normal = 'N'
+    doctor = 'D'
+
+
 class User(db.Model):
     __tablename__ = 'users'
     # id
     id = db.Column(db.String(36), primary_key=True, default=str(uuid.uuid4()))
-    first_name = db.Column(db.String(100))
-    last_name = db.Column(db.String(100))
+    first_name = db.Column(db.String(100), nullable=False)
+    last_name = db.Column(db.String(100), nullable=False)
     phone = db.Column(db.String(11), unique=True, nullable=False)
     email = db.Column(db.String(256), unique=True, nullable=False)
     slug = db.Column(db.String(200), unique=True, nullable=False)
     affiliate_id = db.Column(db.String(10), unique=True, default=generated_uuid())
-    address = db.Column(db.Text())
+    affiliated_by = db.Column(db.String(10), nullable=True)
+    location = db.Column(db.Text(), nullable=True)
+    address = db.Column(db.Text(), nullable=True)
     password = db.Column(db.String(500), nullable=False)
     verify = db.Column(db.DateTime(), default=datetime.datetime.now())
-    verify_by_admin = db.Column(db.DateTime())
+    verify_by_admin = db.Column(db.Boolean(), default=False)
+    blood_type = db.Column(db.Enum(BloodType), default=BloodType.Not_given)
+    type = db.Column(db.Enum(Type), default=Type.normal)
