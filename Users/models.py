cc0f854b12cf59387433911e4354da953d1a462d
pedram.zamaninezhad <pzamaninejad@yahoo.com>
2024-02-04 09:16:36 +0330

    implementing part of laboratories and theire branches

--- Users/models.py
+++ Users/models.py
@@ -4,6 +4,7 @@ from enum import Enum
 from shortuuid import ShortUUID
 
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship
 
 db = SQLAlchemy()
 
@@ -42,26 +43,33 @@ class User(db.Model):
     email = db.Column(db.String(256), unique=True, nullable=False)
     slug = db.Column(db.String(200), unique=True, nullable=False)
     sex = db.Column(db.Enum(Sex), default=Sex.not_given)
-    weight = db.Column(db.Integer(), nullable=True)
+    weight = db.Column(db.Float, nullable=True)
     affiliate_id = db.Column(db.String(10), unique=True, default=ShortUUID().random(length=10))
     affiliated_by = db.Column(db.String(10), nullable=True)
-    location = db.Column(db.Text(), nullable=True)
-    address = db.Column(db.Text(), nullable=True)
+    location = db.Column(db.Text, nullable=True)
+    address = db.Column(db.Text, nullable=True)
     password = db.Column(db.String(500), nullable=False)
-    verify = db.Column(db.DateTime(), default=datetime.datetime.now())
-    verify_by_admin = db.Column(db.Boolean(), default=False)
+    verify = db.Column(db.DateTime, default=datetime.datetime.now())
+    verify_by_admin = db.Column(db.Boolean, default=False)
     blood_type = db.Column(db.Enum(BloodType), default=BloodType.Not_given)
     user_type = db.Column(db.Enum(UserType), default=UserType.normal)
 
 
 class Laboratory(db.Model):
     __tablename__ = 'laboratory'
-    ...
+
+    id = db.Column(db.String(36), primary_key=True, default=str(uuid.uuid4()))
+    name = db.Column(db.String(256), nullable=False)
+    employee = db.Column(db.Integer, nullable=True)
+    year_founded = db.Column(db.Date, nullable=False)
+    branches = relationship('laboratory_branch', backref='laboratory', lazy=True)
 
 
 class LabBranch(db.Model):
     __tablename__ = 'laboratory_branch'
-    ...
+    id = db.Column(db.Integer, primary_key=True)
+    laboratory_id = db.Column(db.String(36), db.ForeignKey('laboratory.id'), nullable=False)
+    branch_name = db.Column(db.String(256))
 
 
 class Address(db.Model):
